925d4c1d80a615e3e037405e8b0784bdf8d05c19
Cecep Saeful Azhar Hidayat, ST <122300501+cecep-azhar@users.noreply.github.com>
2026-01-10 20:39:48 +0700

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone', // <--- WAJIB ADA UNTUK COOLIFY/DOCKER
   reactStrictMode: true,
   typedRoutes: true
 };
